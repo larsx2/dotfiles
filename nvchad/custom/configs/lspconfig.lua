a0bdb0ac8383cada00e4191b263ac26a9eabc191
Eduardo Urias <edw.urias@gmail.com>
2024-10-28 07:18:39 -0700

    chore(nvim): update custom config

--- nvchad/custom/configs/lspconfig.lua
+++ nvchad/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd" }
+local servers = { "html", "cssls", "tsserver", "clangd", "yamlls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -14,4 +14,21 @@ for _, lsp in ipairs(servers) do
 end
 
 -- 
--- lspconfig.pyright.setup { blabla}
+--
+-- lspconfig.graphql-lsp.setup
+local util = require("lspconfig/util")
+lspconfig.graphql.setup {
+  cmd = { "graphql-lsp", "server", "-m", "stream" },
+  filetypes = { "graphql", "typescriptreact", "javascriptreact" },
+  root_dir = function(fname)
+    return util.root_pattern('.git')(fname) or util.path.dirname(fname)
+  end,
+}
+
+lspconfig.terraformls.setup{}
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.tf", "*.tfvars"},
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
