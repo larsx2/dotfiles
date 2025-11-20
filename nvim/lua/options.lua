abd0f349fac3372798c9c6167b0f1e994aac4e70
Eduardo Urias <edw.urias@gmail.com>
2025-11-20 10:19:44 -0700

    chore: enable ctrl + j/k on telescope picker

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -22,3 +22,20 @@ vim.opt.diffopt = {
   "indent-heuristic",
   "iwhite", -- toggle when needed
 }
+
+-- Example from docs / issues
+local actions = require "telescope.actions"
+require("telescope").setup {
+  defaults = {
+    -- ...
+  },
+  pickers = {
+    find_files = {
+      attach_mappings = function(_, map)
+        map("i", "<C-j>", actions.move_selection_next)
+        map("i", "<C-k>", actions.move_selection_previous)
+        return true
+      end,
+    },
+  },
+}
